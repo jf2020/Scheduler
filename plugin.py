ece5e14de09bd027ad7b3a3981095c328a7fb2cd
Jean-Francois Cosse <Jean-francois.cosse@devops.ncia.nato.int>
2021-07-19 21:40:28 +0200

    device starts at 1

--- plugin.py
+++ plugin.py
@@ -227,7 +227,7 @@ class BasePlugin:
             Domoticz.Error("The number of Heating Switches doesn't match the number of Zones")
 
         #delete if too many devices or wrong device type
-        for i in range(len(Devices), 0, -1) :
+        for i in range(len(Devices), 1, -1) :
             if i > len(zoneNames) * 2 :
                 Devices[i].Delete()
             elif i % 2 == 1 and Devices[i].Type != 242 :

97d07545a67ee72aafaf579eb58321bd21f5211c
Jean-Francois Cosse <Jean-francois.cosse@devops.ncia.nato.int>
2021-07-20 09:43:32 +0200

    sss

--- plugin.py
+++ plugin.py
@@ -633,7 +633,7 @@ def JsonToTimers(device, data, plugin, pluginDevice):
         if day == "zone":
             continue
         if day == "temps":
-            pluginDevice.Update(nValue=pluginDevice.nValue, sValue=pluginDevice.sValue, Description = str(plan[day]))
+            pluginDevice.Update(nValue=pluginDevice.nValue, sValue=pluginDevice.sValue, Description="TOTO")
             plugin.Internals['ComfortTemp'] = plan[day]["C"]
             plugin.Internals['EcoTemp'] = plan[day]["E"]
             plugin.Internals['NightTemp'] = plan[day]["N"]

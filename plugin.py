7053deb5a4e6b5e2cbaca5aef5523ea8913bfab5
Fab2517 <fab.cosse@gmail.com>
2021-07-20 13:16:09 +0200

    bug JF

--- plugin.py
+++ plugin.py
@@ -234,8 +234,8 @@ class BasePlugin:
             Domoticz.Error("The number of Heating Switches doesn't match the number of Zones")
 
         #delete if too many devices or wrong device type
-        for i in range(len(Devices), 1, -1) :
-            Devices[i].Delete()
+        # for i in range(len(Devices), 1, -1) :
+        #     Devices[i].Delete()
 
         # for i in range(len(Devices) - 1, 0, -1) :
         #     if i > len(zoneNames) * 2 :
